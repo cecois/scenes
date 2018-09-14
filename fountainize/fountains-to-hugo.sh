5688a3804a2358facc19ad41436677ae140183f8
cecois <cecmcgee@gmail.com>
2018-09-14 19:47:08 -0400

    fountain htmls working

--- fountainize/fountains-to-hugo.sh
+++ fountainize/fountains-to-hugo.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+DB_FOUNTAINS=$(cat .env.json | jq -r '.DB_FOUNTAINS')
+DB_HTML_RAW=$(cat .env.json | jq -r '.DB_HTML_RAW')
+DB_FM_RAW=$(cat .env.json | jq -r '.DB_FM_RAW')
+DB_HTML=$(cat .env.json | jq -r '.DB_HTML')
 
-
-for F in $(ls ${DB_FOUNTAINS}f11.fountain);
+for F in $(ls ${DB_FOUNTAINS}*.fountain);
 do
-  # N="${$(basename ${F})%.*}";
+	echo "processing $F..."
   N=$(basename ${F} | tr "." " "|awk '{print $1}');
 textplay <$F >$DB_HTML_RAW$N.html;
 cat $DB_FM_RAW$N.json>${DB_HTML}$N.html;
